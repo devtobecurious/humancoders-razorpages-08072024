4bee6f4267cc5a449bc85b7d10ee796a902a5141
Evan BOISSONNOT <evan@boissonnot.fr>
2024-07-08 11:50:11 +0200

    feat: add people page

---
+++ SdA.AFrodoJourney/SdA.AFrodoJourney.Core.Models/People.cs
@@ -0,0 +1,14 @@
+﻿namespace SdA.AFrodoJourney.Core.Models
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class People
+    {
+        #region Properties
+        public int Id { get; set; }
+
+        public string Surname { get; set; } = string.Empty;
+        #endregion
+    }
+}
---
+++ SdA.AFrodoJourney/SdA.AFrodoJourney.Web.UI/Pages/Characters/People.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SdA.AFrodoJourney.Web.UI.Pages.Characters
+{
+    public class PeopleModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
